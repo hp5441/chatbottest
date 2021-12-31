f8dfdfe4bc3c30519dd533995211499fe40dbaa9
ardhas-ilearn <ilearnportal@ardhas.com>
2021-12-31 15:19:59 +0530

    chat bot initial

--- sql_controller/database.py
+++ sql_controller/database.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from config import Settings
+
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/db"
+settings = Settings()
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.postgres_user}:{settings.postgres_pass}@{settings.postgres_host}:5432/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
