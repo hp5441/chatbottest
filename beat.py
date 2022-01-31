2fca01568dd436658038653270d614b7ba78331f
ardhas-ilearn <ilearnportal@ardhas.com>
2022-01-31 22:07:36 +0530

    added cron job

--- beat.py
+++ beat.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-sched = BlockingScheduler()
+sched = BlockingScheduler(timezone="Asia/Calcutta")
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
