6d7423029612286b8e66487c2981839a79fcf5ea
ardhas-ilearn <ilearnportal@ardhas.com>
2022-01-31 18:51:40 +0530

    added cron job

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker
+beat: python beat.py
---
+++ beat.py
@@ -0,0 +1,7 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('app is up')
