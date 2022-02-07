f530aa661e5d44c98acec4c2eb5a8dea6f11b786
ardhas-ilearn <ilearnportal@ardhas.com>
2022-02-07 14:23:09 +0530

    modified process question

--- main.py
+++ main.py
@@ -73,8 +73,9 @@ def process_question(question: models.Question, match=""):
         if pattern_index is not None:
             processed_question = processed_question[:pattern_index]+"<em>"+processed_question[pattern_index:pattern_index+len(match)]+"</em>"+processed_question[pattern_index+len(match):]
             print(processed_question)
+    print(question.is_keyword)
     if question.is_keyword:
-        question.question = "Related Info On : "+ processed_question
+        processed_question = "Related Info On : "+ processed_question
     question.question = textile(processed_question, html_type="html5")
     return question
 
